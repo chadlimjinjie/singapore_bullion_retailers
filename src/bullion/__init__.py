749f03dcf512cc30b4b6ae9078c4bcc21f1548bd
Chad Lim <chadlimjinjie@gmail.com>
2024-10-06 15:12:30 +0800

    renamed:    src/bullion/stargrams.py -> src/bullion/StarGrams/__init__.py 	modified:   src/bullion/__init__.py

--- src/bullion/__init__.py
+++ src/bullion/__init__.py
@@ -1,15 +1,16 @@
-import bullion.silverbullion, bullion.stargrams
+import bullion.silverbullion
 from .BullionStar import BullionStar
+from .StarGrams import StarGrams
 
 async def login(email: str, password: str, retailer: str):
 
     match retailer:
         case "bullionstar":
-            result = BullionStar().login(email, password)
+            result = BullionStar.login(email, password)
         case "silverbullion":
             result = await bullion.silverbullion.login(email, password)
         case "stargrams":
-            result = await bullion.stargrams.StarGrams().login(email, password)
+            result = await StarGrams.login(email, password)
 
     return result
 
